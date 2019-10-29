203f7547bbe5f6fedf7a63d64b36ced72b44a7df
forderation <kharisma.muzaki@gmail.com>
2019-10-29 10:55:08 +0700

    design front auth

--- app/src/main/java/com/muzaki/protypeonlineshop/LoginActivity.java
+++ app/src/main/java/com/muzaki/protypeonlineshop/LoginActivity.java
@@ -2,6 +2,7 @@ package com.muzaki.protypeonlineshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,17 +10,18 @@ import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private Button btnSignIn;
     private EditText edtUsername, edtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        btnSignIn = findViewById(R.id.btn_login);
+        Button btnSignIn = findViewById(R.id.btn_login);
+        Button btnSignUp = findViewById(R.id.btn_sign_up);
         edtUsername = findViewById(R.id.edt_username);
         edtPassword = findViewById(R.id.edt_password);
         btnSignIn.setOnClickListener(this);
+        btnSignUp.setOnClickListener(this);
     }
 
     @Override
@@ -32,6 +34,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             if(edtPassword.getText().toString().isEmpty()){
                 edtPassword.setError("Password tidak boleh kosong");
             }
+        }else if(id==R.id.btn_sign_up){
+            startActivity(new Intent(this,RegisterActivity.class));
         }
     }
 }
